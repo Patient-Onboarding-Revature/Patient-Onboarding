974037bd68935b9f66c3aabf1075d93611ffae2e
Miotis2004 <miotis2004@gmail.com>
2019-09-18 11:17:45 -0400

    Fixed jenkins build command

--- devops/JenkinsShell.sh
+++ devops/JenkinsShell.sh
@@ -11,6 +11,6 @@ sudo systemctl enable docker
 
 curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install jenkins
+sudo yum -y install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
